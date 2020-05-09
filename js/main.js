b8ea7bad41df427b40d988ddc93f898a8b79c286
Halisson Alves <halisson.eng@gmail.com>
2020-05-09 02:13:21 -0300

    Implementação de Chart.js e Organização na estrutura de dados

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
-$(document).ready( function () {
-  $('#myTable').DataTable();
-} );
+var gamesData = [];
+
+$(document).ready(function () {
+
+  $.ajax({
+    url: "../data/games.json", success: function (result) {
+      console.log(result)
+      result.games.map(el => {
+        gamesData.push(el);
+      });
+      console.log(gamesData)
+      addRowFunc();
+    }
+  });
+
+  addRowFunc = function () {
+    var t = $('#myTable').DataTable();
+    console.log(gamesData[1].name)
+
+    for (let index = 0; index < gamesData.length; index++) {
+      t.row.add([
+        gamesData[index].name,
+        gamesData[index].platform,
+        gamesData[index].genre,
+        gamesData[index].date,
+        gamesData[index].score,
+        gamesData[index].dificulty,
+      ]).draw(false);
+
+    }
+  }
+
+});
